1659c0052a4dea8145073b9fdada7f9181eb5af9
Christophe Hoste <christophe.hoste@gmail.com>
2020-04-04 01:00:53 +0200

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,14 @@ export default () => (
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
-      <title>Christophe Hoste</title>
-      <meta name="title" content="Christophe Hoste" />
-      <meta name="description" content="Christophe Hoste - Stuttgart, Germany." />
+      <title>Christophe Hoste - Portfolio</title>
+      <meta name="title" content="Christophe Hoste - Software Developer(iOS/Front-End)" />
+      <meta name="description" content="I'm Chris a Software-Developer (iOS/Front-End) based in Stuttgart, Germany. Also, passionate about User Experience (UX) / User Research and Usability Engineering. Love building rich interface applications with superior UX in mind. Check out my portfolio and feel free to contact me." />
       <link rel="canonical" href="https://chrishoste.com" />
 
-      <meta name="application-name" content="Christophe Hoste" />
+      <meta name="application-name" content="Christophe Hoste - Portfolio" />
       <meta name="theme-color" content="#342D3D" />
-      <meta name="apple-mobile-web-app-title" content="Christophe Hoste" />
+      <meta name="apple-mobile-web-app-title" content="Christophe Hoste - Portfolio" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="mobile-web-app-capable" content="yes" />
