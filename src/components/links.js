862efa96f9919efc1e023935518f16998e9fd73c
Christophe Hoste <christophe.hoste@gmail.com>
2020-04-04 00:04:01 +0200

    Added Accessibility

--- src/components/links.js
+++ src/components/links.js
@@ -8,13 +8,13 @@ import In from "../img/in.svg";
 export default () => (
   <div className={styles.links}>
     <OutboundLink href="https://github.com/chrishoste" rel="noopener noreferrer" target="_blank" aria-label="Link to my GitHub profile">
-      <Github alt="" />
+      <Github alt="GitHub Icon" />
     </OutboundLink>
     <OutboundLink href="https://github.com/chrishoste" rel="noopener noreferrer" target="_blank" aria-label="Link to my Xing profile">
-      <Mail alt="" />
+      <Mail alt="Envelope Icon for mail" />
     </OutboundLink>
     <OutboundLink href="https://www.xing.com/profile/Christophe_Hoste2" rel="noopener noreferrer" target="_blank" aria-label="Link to my Xing profile">
-      <In alt="" />
+      <In alt="LinkedIn Icon" />
     </OutboundLink>
   </div>
 );
